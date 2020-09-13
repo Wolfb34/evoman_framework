24cf7664a95f4f251ea7865cbdee8b5c03b68d76
Wolf bij 't Vuur <wolfatthefire@hotmail.com>
2020-09-13 15:57:48 +0200

    initial recombinator functions - blend, 1-point

--- Task1/task1_recombination.py
+++ Task1/task1_recombination.py
@@ -1,2 +1,92 @@
+import numpy as np
+import itertools
 
-#???
+class recombination:
+
+    """Choose pairs of parents to create offspring until the population is filled.
+       To create children, choose a cut-off point and use the first parent to the left,
+       second parent to the right for first child. Reverse for second. Returns
+       the new population.
+       parents - all the parents that were selected to reproduce
+       npop - the total population"""
+    def simple(self, parents, npop):
+        nvar = len(parents[0])
+        next_gen = np.zeros([npop, nvar])
+        nums = [i for i in range(len(parents))]
+        pairs = [pair for pair in itertools.combinations(nums, 2)]
+        if len(pairs) < npop:
+            print("Not enough pairs for a full generation, exiting")
+            return None
+
+        for i in range(0, npop, 2):
+            print(i)
+            index = np.random.choice(nums)
+            nums.remove(index)
+            donor1 = parents[pairs[index][0]]
+            donor2 = parents[pairs[index][1]]
+
+            cut_point = np.random.randint(1, nvar - 1)
+
+            child1 = []
+            child2 = []
+            for j in range(nvar):
+                if j < cut_point:
+                    child1.append(donor1[j])
+                    child2.append(donor2[j])
+                else:
+                    child1.append(donor2[j])
+                    child2.append(donor1[j])
+
+            print(child1)
+            next_gen[i] = child1
+            if i + 1 < npop:
+                next_gen[i + 1] = child2
+        return next_gen
+
+    """Choose pairs of parents to create offspring until the population is filled.
+       To create children, find the differences between every entry of the parents,
+       then pick an alpha between 0 and 1 and create children by choosing elements
+       between min_i - alpha * diff_i and min_i + alpha * diff_i. Returns
+       the new population.
+       parents - all the parents that were selected to reproduce
+       npop - the total population"""
+    def blend(self, parents, npop):
+        nvar = len(parents[0])
+        next_gen = np.zeros([npop, nvar])
+        nums = [i for i in range(len(parents))]
+        pairs = [pair for pair in itertools.combinations(nums, 2)]
+        if len(pairs) < npop:
+            print("Not enough pairs for a full generation, exiting")
+            return None
+
+        for i in range(0, npop, 2):
+            index = np.random.choice(nums)
+            nums.remove(index)
+
+            donor1 = parents[pairs[index][0]]
+            donor2 = parents[pairs[index][1]]
+
+            minarray = list(map(min, donor1, donor2))
+            maxarray = list(map(max, donor1, donor2))
+
+            diff = list(map(lambda x, y: x - y, maxarray, minarray))
+            alpha = np.random.uniform(0, 1)
+
+            low = [minarray[i] - alpha * diff[i] for i in range(nvar)]
+            high = [minarray[i] + alpha * diff[i] for i in range(nvar)]
+
+            child1 = np.random.uniform(low, high)
+            next_gen[i] = child1
+            if i + 1 < npop:
+                child2 = np.random.uniform(low, high)
+                next_gen[i + 1] = child2
+        return next_gen
+
+
+NVAR = 4
+NPOP = 4
+parent_array = np.random.uniform(0, 1, (NPOP, NVAR))
+print(parent_array)
+recon = recombination()
+new_gen = recon.blend(parent_array, 6)
+print(new_gen)
