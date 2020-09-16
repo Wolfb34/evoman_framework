a2f80eea98b0742eb47780e7f0555370cd17225c
Wolf bij 't Vuur <wolfatthefire@hotmail.com>
2020-09-16 12:45:21 +0200

    start work on recording best solutions

--- task1_constants.py
+++ task1_constants.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 N_HIDDEN_NEURONS = 10
+RECORD = False
 NPOP = 15
 NGEN = 10
 MIN_DEV = 0.0001 #picked at random, do research into what is best.
 ROTATION_MUTATION = np.radians(5)
 STANDARD_DEVIATION = 0.04 #picked at random
 DOM_L = -1
-DOM_U = 1 
+DOM_U = 1
--- task1_specialist.py
+++ task1_specialist.py
@@ -22,22 +22,29 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 
-env = Environment(experiment_name=experiment_name,level=2,
+env = Environment(experiment_name=experiment_name,level=2, enemies=[8],
                   player_controller=player_controller(N_HIDDEN_NEURONS),
                   speed="fastest")
+N_VARS = (env.get_num_sensors() + 1) * N_HIDDEN_NEURONS + (N_HIDDEN_NEURONS + 1) * 5
+
 init = Initialization(DOM_L, DOM_U)
 evaluator = Evaluation(env)
 selector = Selection()
 recombinator = Recombination()
 mutator = Mutation(MIN_DEV, ROTATION_MUTATION, STANDARD_DEVIATION, DOM_L, DOM_U)
 
-n_vars = (env.get_num_sensors() + 1) * N_HIDDEN_NEURONS + (N_HIDDEN_NEURONS + 1) * 5
-
-
-population = init.uniform_initialization(NPOP, n_vars)
+population = init.uniform_initialization(NPOP, N_VARS)
+avg_fit = []
+max_fit = []
 for i in range(NGEN):
     print(i)
     fitness_list = evaluator.simple_eval(population)
+    avg_fit.append(np.average(fitness_list))
+    max_fit.append(np.amax(fitness_list))
     parents = selector.select_best_percentage(population, fitness_list)
+    if i == NGEN - 1:
+        best = selector.select_best_n(population, 1)
     population = recombinator.simple(parents, NPOP)
     population = mutator.nonuniform_mutation(population)
+
+# if RECORD:
