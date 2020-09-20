34a3799bdb7f175b52e17261105757d6adba4e33
cek270 <cek270@student.vu.nl>
2020-09-20 11:11:34 +0200

    Logger now makes separate file for maximum and average performance

--- task1_logger.py
+++ task1_logger.py
@@ -8,19 +8,26 @@ class Logger:
 
     def __init__(self, experiment_name):
         self.experiment_name = experiment_name
-        self.log_file = "Logs/Task1/{name:s}/log.txt".format(name=self.experiment_name)
+        self.log_file_avg = "Logs/Task1/{name:s}/log_avg.txt".format(name=self.experiment_name)
+        self.log_file_max = "Logs/Task1/{name:s}/log_max.txt".format(name=self.experiment_name)
+
 
         directory = "Logs/Task1/{name:s}".format(name=experiment_name)
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        file = open(self.log_file, "a")
-        file.write("----Start new run----\n")
+        file_avg = open(self.log_file_avg, "a")
+        file_max = open(self.log_file_max, "a")
 
+        file_avg.write("\n")
+        file_max.write("\n")
 
     def log_results(self, fitness):
-        file = open(self.log_file, "a")
-        file.write("%.{n}f,%.{n}f\n".format(n=SIGNIF) %(self.__average__(fitness), self.__max__(fitness)))
+        file_avg = open(self.log_file_avg, "a")
+        file_max = open(self.log_file_max, "a")
+
+        file_avg.write("%.{n}f, ".format(n=SIGNIF) % (self.__average__(fitness)))
+        file_max.write("%.{n}f, ".format(n=SIGNIF) % (self.__max__(fitness)))
 
     def __average__(self, np_array):
         return np_array.mean()
