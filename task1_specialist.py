6ea2850b821e19cd9fe4a77507ed3882c26e73ce
Wolf bij 't Vuur <wolfatthefire@hotmail.com>
2020-09-20 11:12:22 +0200

    make enemy constant and add ig calculation

--- task1_specialist.py
+++ task1_specialist.py
@@ -14,6 +14,7 @@ from task1_recombination import Recombination
 from task1_initialization import Initialization
 from task1_evaluation import Evaluation
 from task1_constants import *
+from task1_logger import Logger
 from demo_controller import player_controller
 from environment import Environment
 
@@ -22,7 +23,7 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 
-env = Environment(experiment_name=experiment_name,level=2, enemies=[8],
+env = Environment(experiment_name=experiment_name,level=2, enemies=ENEMY,
                   player_controller=player_controller(N_HIDDEN_NEURONS),
                   speed="fastest")
 N_VARS = (env.get_num_sensors() + 1) * N_HIDDEN_NEURONS + (N_HIDDEN_NEURONS + 1) * 5
@@ -30,21 +31,24 @@ N_VARS = (env.get_num_sensors() + 1) * N_HIDDEN_NEURONS + (N_HIDDEN_NEURONS + 1)
 init = Initialization(DOM_L, DOM_U)
 evaluator = Evaluation(env)
 selector = Selection()
+logger = Logger(experiment_name)
 recombinator = Recombination()
 mutator = Mutation(MIN_DEV, ROTATION_MUTATION, STANDARD_DEVIATION, DOM_L, DOM_U)
 
 population = init.uniform_initialization(NPOP, N_VARS)
-avg_fit = []
-max_fit = []
+
 for i in range(NGEN):
     print(i)
     fitness_list = evaluator.simple_eval(population)
-    avg_fit.append(np.average(fitness_list))
-    max_fit.append(np.amax(fitness_list))
+    logger.log_results(fitness_list)
     parents = selector.select_best_percentage(population, fitness_list)
     if i == NGEN - 1:
-        best = selector.select_best_n(population, 1)
+        best = selector.select_best_n(population, fitness_list, 1)
     population = recombinator.simple(parents, NPOP)
     population = mutator.nonuniform_mutation(population)
 
-# if RECORD:
+ig_list = []
+for i in range(5):
+    fitness, p_health, e_health, tim = env.play(pcont=best)
+    ig_list.append(p_health - e_health)
+print(ig_list)
