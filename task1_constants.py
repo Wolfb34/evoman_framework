4fedb0dca95fe02f94f8a6f04f98ba01f62d3563
cek270 <cek270@student.vu.nl>
2020-09-23 10:37:17 +0200

    Change SD

--- task1_constants.py
+++ task1_constants.py
@@ -5,9 +5,9 @@ NPOP = 20
 NGEN = 10
 MIN_DEV = 0.0001 #picked at random, do research into what is best.
 ROTATION_MUTATION = np.radians(5)
-STANDARD_DEVIATION = 0.04 #picked at random
+STANDARD_DEVIATION = 0.32 #picked at random
 DOM_L = -1
 DOM_U = 1
-ENEMY = [2]
+ENEMY = [6]
 
 INIT_SD = 0.0001
