1449309e7e1cfe268d48598d7cd3a6ee3d45de04
Erik Link <erik.link@live.nl>
2020-09-30 12:06:03 +0200

    added method for evaluating multiple mode

--- task1_evaluation.py
+++ task1_evaluation.py
@@ -13,3 +13,12 @@ class Evaluation:
             fitness_results[i] = fitness
 
         return fitness_results
+
+    def multiplemode_eval(self, pop):
+        length = pop.shape[0]
+        fitness_results = np.empty(length)
+        for i in range(length):
+            fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
+            fitness_results[i] = np.sum(fitness)
+
+        return fitness_results
