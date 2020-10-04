697fa340a53e0d95e2031bf5bc49f0c4b888221c
Erik Link <erik.link@live.nl>
2020-10-04 13:05:50 +0200

    updates evaluation

--- task1_evaluation.py
+++ task1_evaluation.py
@@ -10,21 +10,39 @@ class Evaluation:
     def simple_eval(self, pop):
         length = pop.shape[0]
         fitness_results = np.empty(length)
-        for enemy in self.enemies:
-            self.env.update_parameter('enemies', [enemy])
-            for i in range(length):
-                fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
-                fitness_results[i] += fitness
+        for i in range(length):
+            fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
+            fitness_results[i] = fitness
 
         return fitness_results
+
+    def __share_of__(self, ind1, ind2):
+        dist = np.linalg.norm(ind1 - ind2)
+        if dist > self.share_size:
+            return 0
+        return 1 - dist / self.share_size
+
+    def share_fitness(self, pop, fitness):
+        new_fitness = []
+        length = len(pop)
+        for i in range(length):
+            divisor = sum([self.__share_of__(pop[i], pop[j]) for j in range(length)])
+            new_fitness.append(fitness[i] / divisor)
+        return np.array(new_fitness)
 
     def simple_generalist_eval(self, pop):
         length = pop.shape[0]
         fitness_results = np.empty(length)
         for i in range(length):
             print("\n INDIVIDUAL %d OF %d" %(i+1, length))
-            fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
-            fitness_results[i] = np.sum(fitness)
+            fitness_individual = 0
+
+            for enemy in self.enemies:
+                self.env.update_parameter('enemies', [enemy])
+                fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
+                fitness_individual += fitness
+
+            fitness_results[i] = np.sum(fitness_individual)
 
         return fitness_results
 
@@ -32,8 +50,16 @@ class Evaluation:
         length = pop.shape[0]
         fitness_results = np.empty(length)
         for i in range(length):
-            print("\n INDIVIDUAL %d OF %d" %(i+1, length))
-            fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
-            fitness_results[i] = np.sum(fitness)
+            print("\n INDIVIDUAL %d OF %d" % (i + 1, length))
+            fitness_individual = 0
+
+            for enemy in self.enemies:
+                self.env.update_parameter('enemies', [enemy])
+                fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
+                fitness_individual += fitness
+
+            fitness_results[i] = np.sum(fitness_individual)
+
+        fitness_results = self.share_fitness(pop, fitness_results)
 
         return fitness_results
