d55e9cd3cb49c2bf5c2e4580c7f9cef02c9cbb76
cek270 <cek270@student.vu.nl>
2020-10-04 11:12:33 +0200

    change to constants

--- task1_constants.py
+++ task1_constants.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 N_HIDDEN_NEURONS = 10
-NPOP = 20
-NGEN = 10
-MIN_DEV = 0.0001 #picked at random, do research into what is best.
+NPOP = 100
+NGEN = 50
+MIN_DEV = 0.01 #picked at random, do research into what is best.
 ROTATION_MUTATION = np.radians(5)
 STANDARD_DEVIATION = 0.32 #picked at random
 DOM_L = -1
 DOM_U = 1
 
-INIT_SD = 0.0001
+INIT_SD = 0.32
 
 
 # ------TASK1
