7811ad5ab3a9efa1a52e01551878d78d4b4bd57b
Wolf bij 't Vuur <wolfatthefire@hotmail.com>
2020-10-04 13:07:16 +0200

    remove unused functions

--- task1_evaluation.py
+++ task1_evaluation.py
@@ -16,20 +16,6 @@ class Evaluation:
 
         return fitness_results
 
-    def __share_of__(self, ind1, ind2):
-        dist = np.linalg.norm(ind1 - ind2)
-        if dist > self.share_size:
-            return 0
-        return 1 - dist / self.share_size
-
-    def share_fitness(self, pop, fitness):
-        new_fitness = []
-        length = len(pop)
-        for i in range(length):
-            divisor = sum([self.__share_of__(pop[i], pop[j]) for j in range(length)])
-            new_fitness.append(fitness[i] / divisor)
-        return np.array(new_fitness)
-
     def simple_generalist_eval(self, pop):
         length = pop.shape[0]
         fitness_results = np.empty(length)
