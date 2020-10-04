e140f7ae4eb3ec9a942026013a44d141f5176b1e
Wolf bij 't Vuur <wolfatthefire@hotmail.com>
2020-10-04 12:17:20 +0200

    re-add diagnostic prints

--- task1_evaluation.py
+++ task1_evaluation.py
@@ -15,20 +15,6 @@ class Evaluation:
 
         return fitness_results
 
-    def __share_of__(self, ind1, ind2):
-        dist = np.linalg.norm(ind1 - ind2)
-        if dist > self.share_size:
-            return 0
-        return 1 - dist / self.share_size
-
-    def share_fitness(self, pop, fitness):
-        new_fitness = []
-        length = len(pop)
-        for i in range(length):
-            divisor = sum([self.__share_of__(pop[i], pop[j]) for j in range(length)])
-            new_fitness.append(fitness[i] / divisor)
-        return np.array(new_fitness)
-
     def simple_generalist_eval(self, pop):
         length = pop.shape[0]
         fitness_results = np.empty(length)
@@ -43,6 +29,7 @@ class Evaluation:
         length = pop.shape[0]
         fitness_results = np.empty(length)
         for i in range(length):
+            print("\n INDIVIDUAL %d OF %d" %(i+1, length))
             fitness, player_life, enemy_life, game_run_time = self.env.play(pcont=pop[i])
             fitness_results[i] = np.sum(fitness)
 
